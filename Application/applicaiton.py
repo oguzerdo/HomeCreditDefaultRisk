bf79f0e060f19408aac11c814bd3a521978a8e0d
Oğuz Erdoğan <ouzerdo@gmail.com>
2020-12-06 15:37:34 +0300

    libraries edited

--- Application/applicaiton.py
+++ Application/applicaiton.py
@@ -1,15 +1,8 @@
 import numpy as np
 import pandas as pd
-import gc
 import time
 from contextlib import contextmanager
-from lightgbm import LGBMClassifier
-from sklearn.metrics import roc_auc_score, roc_curve
-from sklearn.model_selection import KFold, StratifiedKFold
-import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
-from sklearn.exceptions import ConvergenceWarning
 from sklearn import preprocessing
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
