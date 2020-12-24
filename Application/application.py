a215a2e7875c2b7daf429047383cc7c15ccd3fba
Oğuz Erdoğan <ouzerdo@gmail.com>
2020-12-25 02:32:24 +0300

    new scripts added

--- Application/application.py
+++ Application/application.py
@@ -27,13 +27,7 @@ def timer(title):
     print("{} - done in {:.0f}s".format(title, time.time() - t0))
 
 
-# One-hot encoding for categorical columns with get_dummies
-def one_hot_encoder(df, nan_as_category=True):
-    original_columns = list(df.columns)
-    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
-    df = pd.get_dummies(df, columns=categorical_columns, dummy_na=nan_as_category)
-    new_columns = [c for c in df.columns if c not in original_columns]
-    return df, new_columns
+
 
 
 # Read data and merge
